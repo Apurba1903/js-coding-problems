d76ac85b88d149f7f45c3aaa9c115a4d14ea6059
Apurba1903 <apurba1903@gmail.com>
2023-08-13 03:27:51 +0600

    slice and splice

---
+++ slice.js
@@ -0,0 +1,6 @@
+const friendsAge = [20, 21, 22, 19, 18, 17, 24, 50, 69, 51];
+const partial = friendsAge.slice(2, 5);
+
+
+console.log(friendsAge);
+console.log(partial);
---
+++ splice.js
@@ -0,0 +1,6 @@
+const friendsAge = [20, 21, 22, 19, 18, 17, 24, 50, 69, 51];
+const partial = friendsAge.splice(2, 3, 77, 88, 99);
+
+
+console.log(friendsAge);
+console.log(partial);
